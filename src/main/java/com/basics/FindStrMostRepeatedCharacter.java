6c4733589ea763767e609c4c4fd3838987d4e165
AlbertXIII <jorge.alberto.rc@gmail.com>
2020-07-02 12:47:18 -0500

    find str most repeated char

---
+++ src/main/java/com/basics/FindStrMostRepeatedCharacter.java
@@ -0,0 +1,32 @@
+package com.basics;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindStrMostRepeatedCharacter {
+
+	public static void main(String[] args) {
+		System.out.println(getMostRepeatedChar("holaooola"));
+	}
+	
+	private static char getMostRepeatedChar(String s) {
+		HashMap<Character, Integer> map = new HashMap<>();
+		for(int i = 0; i < s.length(); i++) {
+			if(map.containsKey(s.charAt(i))) 
+				map.replace(s.charAt(i), map.get(s.charAt(i)) + 1);
+			else map.put(s.charAt(i), 1);
+		}
+		int value = 0;
+		char key = 0;
+		for(Map.Entry<Character,Integer> entry: map.entrySet()) {
+			if(value < entry.getValue()) {
+				value = entry.getValue();
+				key = entry.getKey();
+			}
+		}
+		System.out.println(value);
+		return key;
+		
+	}
+
+}
