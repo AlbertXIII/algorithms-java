269d0fea093e19c5986636a169a3ab640b65ba51
AlbertXIII <jorge.alberto.rc@gmail.com>
2021-06-01 14:40:02 -0500

    feat: 942 impl

--- src/main/java/com/leetcode/nine/DIStringMatch942.java
+++ src/main/java/com/leetcode/nine/DIStringMatch942.java
@@ -1,10 +1,24 @@
 package com.leetcode.nine;
 
+import java.util.Arrays;
+
 public class DIStringMatch942 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(Arrays.toString(diStringMatchN("IDID")));
 	}
+	
+	private static int[] diStringMatchN(String s) {
+        int size = s.length(), low= 0, high = size;
+        int[] res = new int[size + 1];
+        
+        for(int i = 0; i < size; i++){
+            if(s.charAt(i) == 'I') res[i] = low++;
+            else res[i] = high--;
+        }
+        
+        res[size] = high;
+        return res;
+    }
 
 }
